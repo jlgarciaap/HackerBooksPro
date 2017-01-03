c936b7c5ccdc6810f4ea0e9cc64321ccecea19be
Juan Luis Garcia <jlgarciaap@gmail.com>
2017-01-03 23:35:26 +0100

    Initial Commit

---
+++ HackerBooksSuperPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerBooksSuperPro
+//
+//  Created by Juan Luis Garcia on 03/01/2017.
+//  Copyright © 2017 styleapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
